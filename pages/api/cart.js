4b0fbd7de498d6929b602ece98f1224624898b40
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-06-22 00:06:25 +0300

    cart page done

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -6,12 +6,3 @@ export default async function handle(req, res) {
   const ids = req.body.ids;
   res.json(await Product.find({ _id: ids }));
 }
-
-// import {mongooseConnect} from "@/lib/mongoose";
-// import {Product} from "@/models/Product";
-
-// export default async function handle(req,res) {
-//   await mongooseConnect();
-//   const ids = req.body.ids;
-//   res.json(await Product.find({_id:ids}));
-// }
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -2,6 +2,8 @@ import Button from "@/components/Button";
 import { CartContext } from "@/components/CartContext";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import Input from "@/components/Input";
+import Table from "@/components/Table";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
@@ -17,10 +19,41 @@ const Box = styled.div`
   border-radius: 10px;
   padding: 30px;
 `;
+const ProductInfoCell = styled.td`
+  padding: 10px 0;
+`;
+const ProductImageBox = styled.div`
+  width: 100px;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    max-width: 80px;
+    max-height: 80px;
+  }
+`;
+const QuantityLabel = styled.span`
+  padding: 0 8px;
+`;
+const CityHolder = styled.div`
+  display: flex;
+  gap: 5px;
+`;
 
 export default function CartPage({}) {
   const [products, setProducts] = useState([]);
-  const { cartProducts } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+
   useEffect(() => {
     if (cartProducts.length > 0) {
       axios.post("/api/cart", { ids: cartProducts }).then((response) => {
@@ -31,36 +64,130 @@ export default function CartPage({}) {
     }
   }, [cartProducts]);
 
+  function increaseQuantity(id) {
+    addProduct(id);
+  }
+  function decreaseQuantity(id) {
+    removeProduct(id);
+  }
+
+  let totalPrice = 0;
+  for (const productId of cartProducts) {
+    const price = products.find((p) => p._id === productId)?.price || 0;
+    totalPrice += price;
+  }
+
   return (
     <>
       <Header />
-
       <Center>
         <ColWrapper>
           <Box>
+            <h2>Cart:</h2>
             {!cartProducts?.length && (
               <div className="">Your Cart is Empty</div>
             )}
             {products?.length > 0 && (
-              <>
-                <h2>Cart:</h2>
-                {products.map((product) => (
-                  <div>
-                    {product.title}:{" "}
-                    {cartProducts.filter((id) => id === product._id).length}
-                  </div>
-                ))}
-              </>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr>
+                      <ProductInfoCell>
+                        <ProductImageBox>
+                          <img src={product.images[0]} alt="" />
+                        </ProductImageBox>
+                        {product.title}
+                      </ProductInfoCell>
+                      <td>
+                        <Button onClick={() => decreaseQuantity(product._id)}>
+                          -
+                        </Button>
+                        <QuantityLabel>
+                          {
+                            cartProducts.filter((id) => id === product._id)
+                              .length
+                          }
+                        </QuantityLabel>
+                        <Button onClick={() => increaseQuantity(product._id)}>
+                          +
+                        </Button>
+                      </td>
+                      <td>
+                        $
+                        {product.price *
+                          cartProducts.filter((id) => id === product._id)
+                            .length}
+                      </td>
+                    </tr>
+                  ))}
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td>${totalPrice}</td>
+                  </tr>
+                </tbody>
+              </Table>
             )}
           </Box>
           {!!cartProducts?.length && (
             <Box>
               <h2>Order Information</h2>
-              <input type="text" placeholder="Address" />
-              <input type="text" placeholder="Address 2" />
-              <Button block primary>
-                Continue to Payment
-              </Button>
+              <form method="post" action="/api/checkout">
+                <Input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  name="name"
+                  onChange={(ev) => setName(ev.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  name="email"
+                  onChange={(ev) => setEmail(ev.target.value)}
+                />
+                <CityHolder>
+                  <Input
+                    type="text"
+                    placeholder="Country"
+                    value={country}
+                    name="country"
+                    onChange={(ev) => setCountry(ev.target.value)}
+                  />
+                  <Input
+                    type="text"
+                    placeholder="City"
+                    value={city}
+                    name="city"
+                    onChange={(ev) => setCity(ev.target.value)}
+                  />
+                </CityHolder>
+                <Input
+                  type="text"
+                  placeholder="Postal Code"
+                  value={postalCode}
+                  name="postalCode"
+                  onChange={(ev) => setPostalCode(ev.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Street Address"
+                  value={streetAddress}
+                  name="streetAddress"
+                  onChange={(ev) => setStreetAddress(ev.target.value)}
+                />
+                <Button block primary type="submit">
+                  Continue to Payment
+                </Button>
+              </form>
             </Box>
           )}
         </ColWrapper>
